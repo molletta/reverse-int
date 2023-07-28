c8531a70567bfe8085b46b6f4fdbdf210f76488f
molletta <yasyule4ka@mail.ru>
2023-07-28 21:17:46 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let result = '';
+
+    str = n > 0 ? str : str.substring(1);
+    for (i = Number(str.length - 1); i >= 0; i--) {
+    result = Number(result + str[i]);
+    }
+
+    return result
 }
